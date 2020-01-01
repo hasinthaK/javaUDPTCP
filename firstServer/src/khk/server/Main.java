e4b2be0489ea8b8001e9249412a9bcea23266480
hasinthaK <hasiroxsola@gmail.com>
2020-01-01 23:42:31 +0530

    Test TCP server

--- firstServer/src/khk/server/Main.java
+++ firstServer/src/khk/server/Main.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 //        System.out.println("Hello world");
 
 //        server.server1();
 //        udpClient.udpSocket();
 //        udpServer.udpS();
-        html.jEditorPane();
+//        html.jEditorPane();
+        testTCPServer.testTCPServer();
 
     }
 
---
+++ firstServer/src/khk/server/testTCPServer.java
@@ -0,0 +1,50 @@
+package khk.server;
+
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class testTCPServer {
+
+    public static void testTCPServer() throws Exception {
+
+        try{
+
+            ServerSocket tcpServer = new ServerSocket(10010);
+            System.out.println("TCP Server created, Listening..");
+
+            try{
+
+                while(true){
+
+                    Socket socket = tcpServer.accept();
+
+                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                    System.out.println("Writing to client..");
+
+                    Date now = new Date();
+                    Locale locale = new Locale("en", "US");
+                    DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+                    String date = dateFormat.format(now);
+//                    System.out.print(date);
+
+                    outputStream.writeBytes(now.toString());
+                    socket.close();
+
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
